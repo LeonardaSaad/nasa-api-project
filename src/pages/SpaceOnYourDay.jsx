11319696bf88017c52a4186f2dd2c923287392c1
Leonarda Saad <leonardassaad777@gmail.com>
2025-04-11 00:17:10 -0300

    feat: can put a date manually on the "SOYD" page

--- src/pages/SpaceOnYourDay.jsx
+++ src/pages/SpaceOnYourDay.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 
 import DatePicker from "react-datepicker";
@@ -24,6 +24,11 @@ const SpaceOnYourDay = () => {
 
   const { data, error, loading } = useFetchAPI(url);
 
+  /**
+   * The handleClick function formats a selected date and sets a URL with the formatted date for an API
+   * request.
+   * @returns The `handleClick` function is being returned.
+   */
   const handleClick = () => {
     if (!selectedDate) return;
     const formattedDate = format(selectedDate, "yyyy-MM-dd");
@@ -31,6 +36,24 @@ const SpaceOnYourDay = () => {
       `${VITE_API_URL_APOD}?api_key=${VITE_API_KEY}&date=${formattedDate}`,
     );
   };
+
+  const handleChangeRaw = (date) => {
+    if (!date || date.length != 8) return;
+
+    const day = date.substring(0, 2);
+    const month = date.substring(2, 4) - 1;
+    const year = date.substring(4, 8);
+
+    const newDate = new Date(year, month, day);
+    newDate.toLocaleDateString("pt-BR"); // 04/12/2003
+    newDate.toISOString().slice(0, 10); // 2003-12-04 (UTC)
+
+    setSelectedDate(newDate);
+  };
+
+  useEffect(() => {
+    console.log(selectedDate);
+  }, [selectedDate]);
 
   return (
     <div className="flex h-full flex-col">
@@ -44,9 +67,14 @@ const SpaceOnYourDay = () => {
             <DatePicker
               className="font-inter mb-2.5 w-full rounded border-1 border-black text-black"
               placeholderText={t("date")}
+              dateFormat="dd/MM/yyyy"
               selected={selectedDate}
+              onChangeRaw={(event) => {
+                handleChangeRaw(event.target.value);
+              }}
               onChange={(date) => setSelectedDate(date)}
               maxDate={new Date()}
+              showIcon // *NOTE - I will use this?
               showYearDropdown
               showMonthDropdown
             />
@@ -58,7 +86,11 @@ const SpaceOnYourDay = () => {
           </div>
         </div>
 
-        {data ? <MediaContent data={data} error={error} loading={loading} /> : <div></div>}
+        {data ? (
+          <MediaContent data={data} error={error} loading={loading} />
+        ) : (
+          <div></div>
+        )}
       </div>
 
       <Footer />
